dac776e8387283db3649ee86bdc04a857a9dcede
SamoUhliar <samo.uhliar1@gmail.com>
2022-01-25 15:59:03 +0100

    fix web component

--- src/components/CodeText.vue
+++ src/components/CodeText.vue
@@ -1,7 +1,5 @@
 <template>
-    <div style="width:100%;height:100%">
-        <p>dcdc</p>
-    </div>
+    <p>dcmisomcnoisdcnmdsi ko kok kokot kokot nc</p>
 </template>
 <script setup>
 </script>
--- src/main.js
+++ src/main.js
@@ -15,12 +15,20 @@ import '@ionic/vue/css/typography.css';
 import '@ionic/vue/css/padding.css';
 import '@ionic/vue/css/float-elements.css';
 import '@ionic/vue/css/text-alignment.css';
+
 import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './vendor/variables.css';
+
+import { defineCustomElement } from 'vue'
+import CodeText from './components/CodeText'
+
+const MyCodeText = defineCustomElement(CodeText)
+
+customElements.define('code-text', MyCodeText)
 
 const app = createApp(App).use(IonicVue).mount('#app')
 
@@ -29,3 +37,5 @@ Object.keys(IonComponents).forEach(key => {
         app.component(key, IonComponents[key]);
     }
 });
+
+
